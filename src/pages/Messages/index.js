b31b0817eb5ff1f75ead7ccab9377ccd72a22a94
axdr3 <axdber@gmail.com>
2021-09-13 10:46:04 -0300

    layout ok

--- src/pages/Messages/index.js
+++ src/pages/Messages/index.js
@@ -1,50 +1,121 @@
-import React from "react";
-import { Col, Container, Row, Table } from "react-bootstrap";
+import React, { useState, useMemo } from "react";
+import {
+  Col,
+  Container,
+  Row,
+  Table,
+  Button,
+  Form,
+  FloatingLabel,
+} from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import "./styles.css";
+
+const TableRow = ({ data, keys }) => {
+  const memoizedRow = useMemo(
+    () =>
+      Object.fromEntries(
+        Object.entries(data).filter((row) => keys.includes(row[0]))
+      ),
+    [data, keys]
+  );
+  return (
+    <tr>
+      <td>{memoizedRow.trigger}</td>
+      <td>{memoizedRow.channel}</td>
+      <td>{memoizedRow.timer}</td>
+      <td>
+        <Button onClick={() => console.log("id", memoizedRow.id)}>
+          Ver Mensagem
+        </Button>
+      </td>
+    </tr>
+  );
+};
+
+const Select = ({ data, objKey }) => (
+  <Form.Select>
+    <option value=""></option>
+    {data &&
+      data.map((item, index) => (
+        <option key={index + "_" + item.id} value={item[objKey]}>
+          {item[objKey]}
+        </option>
+      ))}
+  </Form.Select>
+);
 
 const MessagesPage = () => {
+  const dispatch = useDispatch();
+  const triggers = useSelector(({ messaging }) => messaging.triggers);
+  const channels = useSelector(({ messaging }) => messaging.channels);
+  const timers = useSelector(({ messaging }) =>
+    messaging.messages.map((message) => ({ timer: message.timer }))
+  );
+  const messages = useSelector(({ messaging }) => messaging.messages);
+
+  const [search, setSearch] = useState("");
+
   return (
     <Container fluid="md">
-      <Row>
-        <Col sm={8}>Mensagens</Col>
-        <Col sm={4}>Pesquisar Nova Mensagem</Col>
+      <Row className="upperRow">
+        <Col sm={8} xs={12}>
+          <h2>Mensagens</h2>
+        </Col>
+        <Col
+          xs={12}
+          sm={4}
+          style={{ display: "flex", justifyContent: "flex-end" }}
+        >
+          <Button variant="dark"> Nova Mensagem</Button>
+        </Col>
       </Row>
-      <Row>
-        <Col sm>Gatilho</Col>
-        <Col sm>Canal</Col>
-        <Col sm>Timer</Col>
-      </Row>
-      <Row>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Gatilho</th>
-              <th>Canal</th>
-              <th>Timer</th>
-              <th>Ações</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td>Larry the Bird</td>
-              <td>Larry the Bird</td>
-              <td>@twitter</td>
-            </tr>
-          </tbody>
-        </Table>
-      </Row>
+      <Form>
+        <Row xs={3} className="upperRow">
+          {triggers && triggers.length > 0 && (
+            <Form.Group as={Col} controlId={"triggerSelect"}>
+              <Form.Label>Gatilho</Form.Label>
+              <Select data={triggers} objKey={"name"} />
+            </Form.Group>
+          )}
+          {channels && channels.length > 0 && (
+            <Form.Group as={Col} controlId={"channelSelect"}>
+              <Form.Label>Canal</Form.Label>
+              <Select data={channels} objKey={"name"} />
+            </Form.Group>
+          )}
+          {timers && timers.length > 0 && (
+            <Form.Group as={Col} controlId={"timerSelect"}>
+              <Form.Label>Timer</Form.Label>
+              <Select data={timers} objKey={"timer"} />
+            </Form.Group>
+          )}
+          <Col xs={12} style={{ padding: "20px 12px" }}>
+            <Button variant="outline-dark"> Pesquisar </Button>
+          </Col>
+        </Row>
+      </Form>
+      {/* <Row> */}
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Gatilho</th>
+            <th>Canal</th>
+            <th>Tempo</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          {messages &&
+            messages.map((message) => (
+              <TableRow
+                data={message}
+                keys={["trigger", "channel", "timer", "id"]}
+              />
+            ))}
+        </tbody>
+      </Table>
+      {/* </Row> */}
     </Container>
   );
 };
