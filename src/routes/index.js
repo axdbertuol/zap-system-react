49287be6f676775d78d634bf72be981d542eaa86
axdr3 <axdber@gmail.com>
2021-09-15 21:15:37 -0300

    privateroute done

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,50 @@
 import { Route, Switch } from "react-router-dom";
-
+import { Redirect } from "react-router";
 import Dashboard from "../pages/Dashboard";
 import LoginPage from "../pages/Login";
 import MessagesPage from "../pages/Messages";
 import NewMessagePage from "../pages/NewMessage";
+import { useSelector } from "react-redux";
+// A wrapper for <Route> that redirects to the login
+// screen if you're not yet authenticated.
+function PrivateRoute({ children, ...rest }) {
+  // const {
+  //   state: { token },
+  // } = useContext(AuthContext);
+  const { isAuthenticated } = useSelector((state) => state.auth);
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        isAuthenticated ? (
+          children
+        ) : (
+          //redirect to Login
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location },
+            }}
+          />
+        )
+      }
+    />
+  );
+}
 
 const Routes = () => {
   return (
     <Switch>
       <Route path="/login" exact render={() => <LoginPage />} />
-      <Route path="/" exact render={() => <Dashboard />} />
-      <Route path="/messages">
+      <PrivateRoute path="/" exact>
+        <Dashboard />
+      </PrivateRoute>
+      <PrivateRoute path="/messages">
         <MessagesPage />
-      </Route>
-      <Route path="/nova-mensagem">
+      </PrivateRoute>
+      <PrivateRoute path="/nova-mensagem">
         <NewMessagePage />
-      </Route>
+      </PrivateRoute>
     </Switch>
   );
 };
